7653e710df35aaaf5d088138bfa002e5f6e39a2f
Manngold <jsw4820@gmail.com>
2021-08-01 21:51:11 +0900

    basic setting

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/app.js',
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['css-loader'],
+      },
+    ],
+  },
+};

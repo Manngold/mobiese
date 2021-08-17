4f36095a5a1226686e5afa104b6f32c80cbb96e8
Manngold <jsw4820@gmail.com>
2021-08-18 01:53:21 +0900

    :rocket: deploy 1.0.0

---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/app.js',
+	},
+	output: {
+		path: path.resolve('./dist'),
+		filename: '[name].js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	plugins: [new Dotenv()],
+};
